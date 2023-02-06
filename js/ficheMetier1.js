ffe931be16874a90bb87e491f4d9a1a69b8489ab
ea-2m <124502937+ea-2m@users.noreply.github.com>
2023-02-06 21:57:43 +0100

    Add files via upload

--- js/ficheMetier1.js
+++ js/ficheMetier1.js
@@ -1,17 +1,17 @@
 const fiche1 = document.getElementById("fiche1")
 
-const I = dessiner_cadre(fiche1, 530, 310, 340, 100, "#a1467e")
+const I = dessiner_cadre(fiche1, 530, 285, 340, 100, "#a1467e")
 ecrire(fiche1, "INGÉNIEUR EN CONCEPTION", I.coordonneesCentre[0], I.coordonneesCentre[1], "white")
 
 // Description
 
-const D1 = dessiner_cadre(fiche1, 860, 150, 200, 60, "#8d1d75")
+const D1 = dessiner_cadre(fiche1, 860, 125, 200, 60, "#8d1d75")
 ecrire(fiche1, "Description", D1.coordonneesCentre[0], D1.coordonneesCentre[1], "white")
-const D2 = dessiner_cadre(fiche1, 860, 250, 200, 60, "#8d1d75")
+const D2 = dessiner_cadre(fiche1, 860, 225, 200, 60, "#8d1d75")
 ecrire(fiche1, "Poser", D2.coordonneesCentre[0], D2.coordonneesCentre[1], "white")
-const D3 = dessiner_cadre(fiche1, 860, 350, 200, 60, "#8d1d75")
+const D3 = dessiner_cadre(fiche1, 860, 325, 200, 60, "#8d1d75")
 ecrire(fiche1, "Étudier", D3.coordonneesCentre[0], D3.coordonneesCentre[1], "white")
-const D4 = dessiner_cadre(fiche1, 860, 450, 200, 60, "#8d1d75")
+const D4 = dessiner_cadre(fiche1, 860, 425, 200, 60, "#8d1d75")
 ecrire(fiche1, "Résoudre", D4.coordonneesCentre[0], D4.coordonneesCentre[1], "white")
 
 dessiner_fleche(fiche1, I.coordonneesNE, D1.coordonneesW)
@@ -21,13 +21,13 @@ dessiner_fleche(fiche1, D3.coordonneesS, D4.coordonneesN)
 
 // Qualités
 
-const Q1 = dessiner_cadre(fiche1, 500, 430, 200, 60, "#b457c4")
+const Q1 = dessiner_cadre(fiche1, 500, 405, 200, 60, "#b457c4")
 ecrire(fiche1, "Qualité nécessaires", Q1.coordonneesCentre[0], Q1.coordonneesCentre[1], "white")
-const Q2 = dessiner_cadre(fiche1, 500, 550, 200, 60, "#b457c4")
+const Q2 = dessiner_cadre(fiche1, 500, 525, 200, 60, "#b457c4")
 ecrire(fiche1, "Rigueur", Q2.coordonneesCentre[0], Q2.coordonneesCentre[1], "white")
-const Q3 = dessiner_cadre(fiche1, 250, 520, 200, 60, "#b457c4")
+const Q3 = dessiner_cadre(fiche1, 250, 495, 200, 60, "#b457c4")
 ecrire(fiche1, "Précision", Q3.coordonneesCentre[0], Q3.coordonneesCentre[1], "white")
-const Q4 = dessiner_cadre(fiche1, 200, 420, 200, 60, "#b457c4")
+const Q4 = dessiner_cadre(fiche1, 200, 395, 200, 60, "#b457c4")
 ecrire(fiche1, "Expertise", Q4.coordonneesCentre[0], Q4.coordonneesCentre[1], "white")
 
 dessiner_fleche(fiche1, I.coordonneesS, Q1.coordonneesN)
@@ -37,15 +37,15 @@ dessiner_fleche(fiche1, Q1.coordonneesW, Q4.coordonneesE)
 
 // Formation
 
-const F1 = dessiner_cadre(fiche1, 500, 100, 200, 60, "#650953")
+const F1 = dessiner_cadre(fiche1, 500, 75, 200, 60, "#650953")
 ecrire(fiche1, "Formation / Diplôme", F1.coordonneesCentre[0], F1.coordonneesCentre[1], "white")
-const F2 = dessiner_cadre(fiche1, 360, 200, 200, 60, "#650953")
+const F2 = dessiner_cadre(fiche1, 360, 175, 200, 60, "#650953")
 ecrire(fiche1, "Licence", F2.coordonneesCentre[0], F2.coordonneesCentre[1], "white")
-const F3 = dessiner_cadre(fiche1, 180, 320, 200, 60, "#650953")
+const F3 = dessiner_cadre(fiche1, 180, 295, 200, 60, "#650953")
 ecrire(fiche1, "Master", F3.coordonneesCentre[0], F3.coordonneesCentre[1], "white")
-const F4 = dessiner_cadre(fiche1, 200, 80, 200, 60, "#650953")
+const F4 = dessiner_cadre(fiche1, 200, 55, 200, 60, "#650953")
 ecrire(fiche1, "Classe préparatoire", F4.coordonneesCentre[0], F4.coordonneesCentre[1], "white")
-const F5 = dessiner_cadre(fiche1, 130, 200, 200, 60, "#650953")
+const F5 = dessiner_cadre(fiche1, 130, 175, 200, 60, "#650953")
 ecrire(fiche1, "École d'Ingénieur", F5.coordonneesCentre[0], F5.coordonneesCentre[1], "white")
 
 dessiner_fleche(fiche1, I.coordonneesN, F1.coordonneesS)
--- js/ficheMetier2.js
+++ js/ficheMetier2.js
@@ -1,19 +1,19 @@
 const fiche2 = document.getElementById("fiche2")
 
-const In = dessiner_cadre(fiche2, 530, 280, 340, 100, "#a1467e")
+const In = dessiner_cadre(fiche2, 530, 250, 340, 100, "#a1467e")
 ecrire(fiche2, "INGÉNIEUR EN CONCEPTION", In.coordonneesCentre[0], In.coordonneesCentre[1], "white")
 
 // Carrière
 
-const C1 = dessiner_cadre(fiche2, 500, 100, 200, 60, "#b457c4")
+const C1 = dessiner_cadre(fiche2, 500, 70, 200, 60, "#b457c4")
 ecrire(fiche2, "Carrière", C1.coordonneesCentre[0], C1.coordonneesCentre[1], "white")
-const C2 = dessiner_cadre(fiche2, 800, 80, 220, 60, "#b457c4")
+const C2 = dessiner_cadre(fiche2, 800, 50, 220, 60, "#b457c4")
 ecrire(fiche2, "Possibilité d’évolution", C2.coordonneesCentre[0], C2.coordonneesCentre[1], "white")
-const C3 = dessiner_cadre(fiche2, 870, 200, 200, 60, "#b457c4")
+const C3 = dessiner_cadre(fiche2, 870, 170, 200, 60, "#b457c4")
 ecrire(fiche2, "Docteur", C3.coordonneesCentre[0], C3.coordonneesCentre[1], "white")
-const C4 = dessiner_cadre(fiche2, 200, 80, 280, 60, "#b457c4")
+const C4 = dessiner_cadre(fiche2, 200, 50, 280, 60, "#b457c4")
 ecrire(fiche2, "Rémunération annuelle brute", C4.coordonneesCentre[0], C4.coordonneesCentre[1], "white")
-const C5 = dessiner_cadre(fiche2, 130, 200, 250, 60, "#b457c4")
+const C5 = dessiner_cadre(fiche2, 130, 170, 250, 60, "#b457c4")
 ecrire(fiche2, "Débutants :", C5.coordonneesCentre[0], C5.coordonneesCentre[1]-12, "white")
 ecrire(fiche2, "de 35 000 € à 40 000 €", C5.coordonneesCentre[0], C5.coordonneesCentre[1]+12, "white")
 
@@ -25,19 +25,19 @@ dessiner_fleche(fiche2, C4.coordonneesS, C5.coordonneesN)
 
 // Conditions de Travail
 
-const T1 = dessiner_cadre(fiche2, 500, 430, 200, 60, "#650953")
+const T1 = dessiner_cadre(fiche2, 500, 400, 200, 60, "#650953")
 ecrire(fiche2, "Conditions de travail", T1.coordonneesCentre[0], T1.coordonneesCentre[1], "white")
-const T2 = dessiner_cadre(fiche2, 870, 450, 200, 60, "#650953")
+const T2 = dessiner_cadre(fiche2, 870, 420, 200, 60, "#650953")
 ecrire(fiche2, "Combien de temps", T2.coordonneesCentre[0], T2.coordonneesCentre[1], "white")
-const T3 = dessiner_cadre(fiche2, 870, 550, 220, 60, "#650953")
+const T3 = dessiner_cadre(fiche2, 870, 520, 220, 60, "#650953")
 ecrire(fiche2, "40 heures par semaine", T3.coordonneesCentre[0], T3.coordonneesCentre[1], "white")
-const T4 = dessiner_cadre(fiche2, 130, 450, 200, 60, "#650953")
+const T4 = dessiner_cadre(fiche2, 130, 420, 200, 60, "#650953")
 ecrire(fiche2, "Avec qui", T4.coordonneesCentre[0], T4.coordonneesCentre[1], "white")
-const T5 = dessiner_cadre(fiche2, 130, 550, 230, 60, "#650953")
+const T5 = dessiner_cadre(fiche2, 130, 520, 230, 60, "#650953")
 ecrire(fiche2, "Une équipe d'ingénieurs", T5.coordonneesCentre[0], T5.coordonneesCentre[1], "white")
-const T6 = dessiner_cadre(fiche2, 390, 520, 200, 60, "#650953")
+const T6 = dessiner_cadre(fiche2, 390, 490, 200, 60, "#650953")
 ecrire(fiche2, "Où", T6.coordonneesCentre[0], T6.coordonneesCentre[1], "white")
-const T7 = dessiner_cadre(fiche2, 620, 550, 200, 60, "#650953")
+const T7 = dessiner_cadre(fiche2, 620, 520, 200, 60, "#650953")
 ecrire(fiche2, "En entreprise", T7.coordonneesCentre[0], T7.coordonneesCentre[1], "white")
 
 dessiner_fleche(fiche2, In.coordonneesS, T1.coordonneesN)
